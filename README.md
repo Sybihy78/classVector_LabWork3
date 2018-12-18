fece94d9c37c9dbeb0103b76b4064524527d3c5b
Sybihy78 <irishka_86.86@mail.ru>
2018-12-18 14:18:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# classVector_LabWork3
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream> //
+#include "Test.h"
+#include "MyVector.h"
+
+int main(int argc, char* argv[])
+{
+	
+	Test t;
+	t.test();
+	/*
+	MyVector vec3 = MyVector();
+	for (int i = 0; i < 32; ++i) {
+		vec3.push_back(i);
+	}
+	std::cout << "size = " << vec3.get_size() << std::endl;
+	std::cout << "capacity = " << vec3.get_capacity() << std::endl;
+	std::cout << std::endl;
+	
+	vec3.push_back(100);
+	std::cout << "size = " << vec3.get_size() << std::endl;
+	std::cout << "capacity = " << vec3.get_capacity() << std::endl;
+	std::cout << std::endl;
+	
+	
+	int myArray[5] = { 0, 1, 2, 3, 4 };
+	MyVector vec = MyVector(myArray, 5);
+	vec.pop_back();
+	std::cout << vec.get_size() << std::endl;
+	MyVector vec1 = MyVector();
+	for (int i = 0; i < 10; ++i) {
+		vec1.push_back(i);
+	}
+	vec1.print();
+	std::cout << std::endl;
+	vec1.pop_back();
+	vec1.print();
+	std::cout << std::endl;
+	int a = vec1.pop_back();
+	std::cout << "a " << a << std::endl;
+	vec1.print();
+	std::cout << std::endl;
+	*/	
+	system("pause"); //
+	return 0;
+}
